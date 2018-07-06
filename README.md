0c92902a5283e88ca7f97f335cd0284ba0a242f1
leonardo <ljcjleonardo@gmail.com>
2018-07-05 23:13:40 -0300

    Colocando e instalando o bootstrap no app :)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Curso_Joao_Ribeiro_Angular_4
 Curso do João Ribeiro de Angular4 no youtube
+
+comando CLI
+
+1 - instalar o bootstrap => npm install --save bootstrap
--- primeroProjetoAngular/src/app/Areas/Area1/area1.ts
+++ primeroProjetoAngular/src/app/Areas/Area1/area1.ts
@@ -1,7 +1,9 @@
 import { Component } from "@angular/core"
 
 @Component({
-    selector: "area1",
+    selector: "area1", // Criar um elemento novo do html
+    // selector: "[area1]", // criar um novo atributo para adicionar ao html
+    // selector: ".area1", // criar um component que funciona como uma class
     templateUrl: "./area1.html",
     styleUrls: ["./area1.css"]
 })
